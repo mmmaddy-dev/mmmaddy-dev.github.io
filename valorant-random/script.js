6a5781cd4c992f5ef22fbcf6e716dfd02698a451
Maddy <80926416+mmmaddy-dev@users.noreply.github.com>
2026-05-17 00:42:21 -0400

    Update script.js

--- valorant-random/script.js
+++ valorant-random/script.js
@@ -210,7 +210,7 @@ function renderAgentPool() {
       }
 
       card.innerHTML = `
-        <img src="valorant-random/images/headshots/${agent.name.toLowerCase()}.png">
+        <img src="images/headshots/${agent.name.toLowerCase()}.png">
 
         <p>${agent.name}</p>
       `;
@@ -413,7 +413,7 @@ function updatePlayerCard(index, agent) {
     slot.querySelector(".player-image");
 
   img.src =
-    `valorant-random/images/full/${agent.name.toLowerCase()}.webp`;
+    `images/full/${agent.name.toLowerCase()}.webp`;
 
   img.classList.remove("hidden");
 
